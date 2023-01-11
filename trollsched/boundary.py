64a6340100aefd05827a0a9de7f3ff6335186639
Martin Raspaud <martin.raspaud@smhi.se>
2023-01-11 12:01:51 +0100

    Add aws support

--- trollsched/boundary.py
+++ trollsched/boundary.py
@@ -21,17 +21,15 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-"""The Boundary classes.
-"""
+"""The SwathBoundary class."""
 
 
 import logging
 import logging.handlers
 
 import numpy as np
-
-from pyresample.boundary import Boundary
 from pyorbital import geoloc, geoloc_instrument_definitions
+from pyresample.boundary import Boundary
 
 logger = logging.getLogger(__name__)
 
@@ -42,18 +40,29 @@ INSTRUMENT = {'avhrr/3': 'avhrr',
 
 
 class SwathBoundary(Boundary):
-
-    """Boundaries for satellite overpasses.
-    """
+    """Boundaries for satellite overpasses."""
 
     def get_instrument_points(self, overpass, utctime,
                               scans_nb, scanpoints, scan_step=1):
-        """Get the boundary points for a given overpass.
-        """
+        """Get the boundary points for a given overpass."""
+        instrument, scan_angle = self.get_instrument_and_angle(overpass)
+
+        sgeom = self.create_instrument_geometry(instrument, scans_nb, scanpoints, scan_step, scan_angle)
+
+        times = sgeom.times(utctime)
+
+        pixel_pos = geoloc.compute_pixels((self.orb.tle._line1,
+                                           self.orb.tle._line2),
+                                          sgeom, times)
+        lons, lats, alts = geoloc.get_lonlatalt(pixel_pos, times)
+
+        del alts
+        return (lons.reshape(-1, len(scanpoints)),
+                lats.reshape(-1, len(scanpoints)))
+
+    def get_instrument_and_angle(self, overpass):
+        """Get the instrument and angle for an overpass."""
         instrument = overpass.instrument
-        # logger.debug("Instrument: %s", str(instrument))
-        # cheating at the moment.
-        # scan_angle = 55.37
         if instrument == "modis":
             scan_angle = 55.0
             instrument = "avhrr"
@@ -69,9 +78,15 @@ class SwathBoundary(Boundary):
         elif instrument.startswith("mersi"):
             scan_angle = 55.4
             instrument = "avhrr"
+        elif overpass.satellite.name.startswith("aws"):
+            scan_angle = 55.25
+            instrument = "avhrr"
         else:
             scan_angle = 55.25
+        return instrument, scan_angle
 
+    def create_instrument_geometry(self, instrument, scans_nb, scanpoints, scan_step, scan_angle):
+        """Create an instrument geometry object."""
         instrument_fun = getattr(geoloc_instrument_definitions,
                                  INSTRUMENT.get(instrument, instrument))
 
@@ -92,21 +107,18 @@ class SwathBoundary(Boundary):
         else:
             logger.warning("Instrument not tested: %s", instrument)
             sgeom = instrument_fun(scans_nb)
-
-        times = sgeom.times(utctime)
-
-        pixel_pos = geoloc.compute_pixels((self.orb.tle._line1,
-                                           self.orb.tle._line2),
-                                          sgeom, times)
-        lons, lats, alts = geoloc.get_lonlatalt(pixel_pos, times)
-
-        del alts
-        return (lons.reshape(-1, len(scanpoints)),
-                lats.reshape(-1, len(scanpoints)))
+        return sgeom
 
     def __init__(self, overpass, scan_step=50, frequency=200):
+        """Initialize the boundary.
+
+        Arguments:
+            overpass: the overpass to use
+            scan_step: how many scans we should skip for a smaller boundary
+            frequency: how much to decimate the top and bottom rows of the boundary.
+        """
         # compute area covered by pass
-        Boundary.__init__(self)
+        super().__init__()
 
         self.overpass = overpass
         self.orb = overpass.orb
@@ -117,50 +129,7 @@ class SwathBoundary(Boundary):
                               (overpass.falltime - overpass.risetime).microseconds / 1000000.0)
 
         logger.debug("Instrument = %s", self.overpass.instrument)
-        if self.overpass.instrument == 'viirs':
-            sec_scan_duration = 1.779166667
-            along_scan_reduce_factor = 1
-        elif self.overpass.instrument.startswith("avhrr"):
-            sec_scan_duration = 1. / 6.
-            along_scan_reduce_factor = 0.1
-        elif self.overpass.instrument == 'ascat':
-            sec_scan_duration = 3.74747474747
-            along_scan_reduce_factor = 1
-            # Overwrite the scan step
-            scan_step = 1
-        elif self.overpass.instrument == 'amsua':
-            sec_scan_duration = 8.
-            along_scan_reduce_factor = 1
-            # Overwrite the scan step
-            scan_step = 1
-        elif self.overpass.instrument == 'mhs':
-            sec_scan_duration = 8./3.
-            along_scan_reduce_factor = 1
-            # Overwrite the scan step
-            scan_step = 1
-        elif self.overpass.instrument == 'mwhs2':
-            sec_scan_duration = 8./3.
-            along_scan_reduce_factor = 1
-            # Overwrite the scan step
-            scan_step = 1
-        elif self.overpass.instrument == 'olci':
-            # 3 minutes of data is 4091 300meter lines:
-            sec_scan_duration = 0.04399902224395014
-            along_scan_reduce_factor = 1
-            # Overwrite the scan step
-            scan_step = 100
-        elif self.overpass.instrument == 'atms':
-            sec_scan_duration = 8/3.
-            along_scan_reduce_factor = 1
-            # Overwrite the scan step
-            scan_step = 1
-
-        else:
-            # Assume AVHRR!
-            logmsg = ("Instrument scan duration not known. Setting it to AVHRR. Instrument: ")
-            logger.info(logmsg + "%s", str(self.overpass.instrument))
-            sec_scan_duration = 1. / 6.
-            along_scan_reduce_factor = 0.1
+        scan_step, along_scan_reduce_factor, sec_scan_duration = self.get_steps_and_duration()
 
         # From pass length in seconds and the seconds for one scan derive the number of scans in the swath:
         scans_nb = scanlength_seconds / sec_scan_duration * along_scan_reduce_factor
@@ -218,19 +187,68 @@ class SwathBoundary(Boundary):
 
         return
 
+    def get_steps_and_duration(self):
+        """Get the steps and duration for the instrument."""
+        if self.overpass.instrument == 'viirs':
+            sec_scan_duration = 1.779166667
+            along_scan_reduce_factor = 1
+        elif self.overpass.instrument.startswith("avhrr"):
+            sec_scan_duration = 1. / 6.
+            along_scan_reduce_factor = 0.1
+        elif self.overpass.instrument == 'ascat':
+            sec_scan_duration = 3.74747474747
+            along_scan_reduce_factor = 1
+            # Overwrite the scan step
+            scan_step = 1
+        elif self.overpass.instrument == 'amsua':
+            sec_scan_duration = 8.
+            along_scan_reduce_factor = 1
+            # Overwrite the scan step
+            scan_step = 1
+        elif self.overpass.instrument == 'mhs':
+            sec_scan_duration = 8./3.
+            along_scan_reduce_factor = 1
+            # Overwrite the scan step
+            scan_step = 1
+        elif self.overpass.instrument == 'mwhs2':
+            sec_scan_duration = 8./3.
+            along_scan_reduce_factor = 1
+            # Overwrite the scan step
+            scan_step = 1
+        elif self.overpass.instrument == 'olci':
+            # 3 minutes of data is 4091 300meter lines:
+            sec_scan_duration = 0.04399902224395014
+            along_scan_reduce_factor = 1
+            # Overwrite the scan step
+            scan_step = 100
+        elif self.overpass.instrument == 'atms':
+            sec_scan_duration = 8/3.
+            along_scan_reduce_factor = 1
+            # Overwrite the scan step
+            scan_step = 1
+
+        else:
+            # Assume AVHRR!
+            logmsg = ("Instrument scan duration not known. Setting it to AVHRR. Instrument: ")
+            logger.info(logmsg + "%s", str(self.overpass.instrument))
+            sec_scan_duration = 1. / 6.
+            along_scan_reduce_factor = 0.1
+        return scan_step, sec_scan_duration, along_scan_reduce_factor
+
     def decimate(self, ratio):
-        l = len(self.top_lons)
-        start = (l % ratio) / 2
-        points = np.concatenate(([0], np.arange(start, l, ratio), [l - 1]))
+        """Remove points from the boundary."""
+        length = len(self.top_lons)
+        start = (length % ratio) / 2
+        points = np.concatenate(([0], np.arange(start, length, ratio), [length - 1]))
 
         self.top_lons = self.top_lons[points]
         self.top_lats = self.top_lats[points]
         self.bottom_lons = self.bottom_lons[points]
         self.bottom_lats = self.bottom_lats[points]
 
-        l = len(self.right_lons)
-        start = (l % ratio) / 2
-        points = np.concatenate(([0], np.arange(start, l, ratio), [l - 1]))
+        length = len(self.right_lons)
+        start = (length % ratio) / 2
+        points = np.concatenate(([0], np.arange(start, length, ratio), [length - 1]))
 
         self.right_lons = self.right_lons[points]
         self.right_lats = self.right_lats[points]
@@ -240,6 +258,7 @@ class SwathBoundary(Boundary):
         return
 
     def contour(self):
+        """Get the contour lon/lats."""
         lons = np.concatenate((self.top_lons,
                                self.right_lons[1:-1],
                                self.bottom_lons,
